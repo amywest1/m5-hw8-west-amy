23cc42802e8d336bc9f07e62b20bbc8b0fd635df
amywest1 <87080188+amywest1@users.noreply.github.com>
2022-03-19 12:46:15 -0400

    added win and loss count text

--- unsolved/index.js
+++ unsolved/index.js
@@ -26,21 +26,12 @@ function chosenBox(event) {
     if (boxNumber == winBox) {
         winCount += 1;
         // if the numbers match, display a winning message by changing the text content of the div#message element
+        document.getElementById("message").innerHTML = "You win!";
+        document.getElementById("wins").innerHTML = "Wins: " + winCount;
     } else {
         lossCount += 1;
         // if the numbers don't match, change the div#message element's text to a random losing message from the array above
+        document.getElementById("message").innerHTML = losing[Math.floor(Math.random()*losing.length)];
+        document.getElementById("losses").innerHTML = "Losses: " + lossCount;
     }
 }
-// within each click event...
-// determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
-
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
-
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
-// if the numbers match, increment wins and display the win count in div#wins
-
-// if the numbers don't match, change the div#message element's text to a random losing message from the array above
-// if the numbers don't match, increment losses and display the loss count in div#losses
